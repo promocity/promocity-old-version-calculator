0aebf2d694f5a3719d99b644349975d614edd721
Артем <verst3.promocity.ekb@yandex.ru>
2015-11-09 17:14:20 +0500

    Страница калькулятора

---
+++ src/js/main.js
@@ -0,0 +1,104 @@
+/*
+ * Third party
+ */
+
+//= partials/jquery-2.1.4.min.js
+/*
+ * Custom
+ */
+//= partials/app.js
+
+$(document).ready(function() {
+    /* calculator.html
+    -----------------------------------------------------------------------------*/
+
+  $(".callback-label").click(function(){
+     $('.callback-img').toggle("fast");
+  });
+
+  $(".call-request-label").click(function(){
+     $('.call-request-img').toggle("fast");
+  });
+
+  $(".search-label").click(function(){
+   $('.search-img').toggle("fast");
+  });
+
+  $(".photo-gallery-label").click(function(){
+   $('.gallery-img').toggle("fast");
+  });
+
+  $(".online-advise-label").click(function(){
+   $('.online-advise-img').toggle("fast");
+  });
+
+  $(".item-catalog-label").click(function(){
+   $('.catalog-img').toggle("fast");
+  });   
+
+  $(".basket-label").click(function(){
+   $('.basket-img').toggle("fast");
+  });
+
+  $(".social-label").click(function(){
+   $('.social-img').toggle("fast");
+  });
+
+  $(".slider-label").click(function(){
+   $('.slider-img').toggle("fast");
+  });
+
+  $(".news-label").click(function(){
+   $('.news-img').toggle("fast");
+  });
+
+  $(".banner-label").click(function(){
+   $('.banner-img').toggle("fast");
+  });
+
+
+
+
+
+
+
+
+
+  $(".audio-label").click(function(){
+   $('.audio-img').toggle("fast");
+  });
+
+  $(".video-label").click(function(){
+   $('.video-img').toggle("fast");
+  });
+
+  $(".private-office-label").click(function(){
+   $('.cabinet-img').toggle("fast");
+  });
+
+  $(".blog-label").click(function(){
+   $('.blog-img').toggle("fast");
+  });
+
+  $(".forum-label").click(function(){
+   $('.forum-img').toggle("fast");
+  });
+
+  $(".robo-label").click(function(){
+   $('.robo-img').toggle("fast");
+  });
+
+  $(".uniq-label").click(function(){
+   $('.uniq-img-wr').toggle("fast");
+  });
+
+
+
+
+
+
+
+
+
+
+});
