879de433031ab4f5e515aa629d05a36e7945d25b
Артем <verst3.promocity.ekb@yandex.ru>
2015-11-10 16:14:22 +0500

    Страница калькулятора переделана в классе block-1 вырезана отдельно кнопка, пункт каталог создан новый в файле catalog-update, изображения в block-1 переставлены для того чтобы заполнить отсутствующий блок catalog

--- src/js/main.js
+++ src/js/main.js
@@ -56,14 +56,6 @@ $(document).ready(function() {
    $('.banner-img').toggle("fast");
   });
 
-
-
-
-
-
-
-
-
   $(".audio-label").click(function(){
    $('.audio-img').toggle("fast");
   });
@@ -92,12 +84,82 @@ $(document).ready(function() {
    $('.uniq-img-wr').toggle("fast");
   });
 
+hideAll();
 
+$(".type-site").change(function(){
+  if($(this).val() == 0) hideAll();
+  if($(this).val() == 1) toggleFormOne();
+  if($(this).val() == 2) toggleFormTwo();
+  if($(this).val() == 3) toggleFormThree();
+  if($(this).val() == 4) toggleFormFour();
+  if($(this).val() == 5) toggleFormFive();
+  //alert($(this).val()); /* debug */
+});
 
+function hideAll() {
+  $('.select-site-visit').hide();
+  $('.select-landing-page').hide();
+  $('.select-corp-site').hide();
+  $('.select-present-site').hide();
+  $('.select-e-shop').hide();
+  $('.starter-img-wr').hide();
+  $('.calc-section').hide();
 
+  $('.callback-img').hide();
+  $('.call-request-img').hide();
+  $('.search-img').hide();
+  $('.gallery-img').hide();
+  $('.online-advise-img').hide();
+  $('.catalog-img').hide();
+  $('.basket-img').hide();
+  $('.social-img').hide();
+  $('.slider-img').hide();
+  $('.news-img').hide();
+  $('.banner-img').hide();
+  $('.audio-img').hide();
+  $('.video-img').hide();
+  $('.cabinet-img').hide();
+  $('.blog-img').hide();
+  $('.forum-img').hide();
+  $('.robo-img').hide();
+  $('.uniq-img-wr').hide();
+  $(".calc-item input[type=checkbox]:checked").css("background", "url(../img/main-content/block-2/checkbox-uncheck.png) no-repeat 50% 50%")
+}
 
+function toggleFormOne() {
+  hideAll();
+  $('.select-site-visit').toggle("fast");
+  $('.calc-section').toggle();
+  $('.starter-img-wr').toggle();
+}
 
+function toggleFormTwo() {
+  hideAll();
+  $('.select-landing-page').toggle("fast");
+  $('.calc-section').toggle();
+  $('.starter-img-wr').toggle();
+}
 
+function toggleFormThree() {
+  hideAll();
+  $('.select-corp-site').toggle("fast");
+  $('.calc-section').toggle();
+  $('.starter-img-wr').toggle();
+}
+
+function toggleFormFour() {
+  hideAll();
+  $('.select-present-site').toggle("fast");
+  $('.calc-section').toggle();
+  $('.starter-img-wr').toggle();
+}
+
+function toggleFormFive() {
+  hideAll();
+  $('.select-e-shop').toggle("fast");
+  $('.calc-section').toggle();
+  $('.starter-img-wr').toggle();
+}
 
 
 
